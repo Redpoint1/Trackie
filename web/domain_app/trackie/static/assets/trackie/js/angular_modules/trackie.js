8dc1bf25922eee0c8fc8eba765ea0fa7c1ef397d
Redpoint1 <Redpoints1@gmail.com>
2016-09-05 18:51:54 +0200

    Added djangoAuth service for AngularJS, unified template for users

--- web/domain_app/trackie/static/assets/trackie/js/angular_modules/trackie.js
+++ web/domain_app/trackie/static/assets/trackie/js/angular_modules/trackie.js
@@ -1,6 +1,6 @@
 (function () {
     "use strict";
-    var trackie_module = angular.module("trackie", ["ngRoute", "ngResource", "restangular"])
+    var trackie_module = angular.module("trackie", ["ngRoute", "ngResource", "ngCookies", "restangular"])
         .config(["$interpolateProvider", function ($interpolateProvider) {
             $interpolateProvider.startSymbol("{$");
             $interpolateProvider.endSymbol("$}");
@@ -12,24 +12,207 @@
         .config(["RestangularProvider", function (RestangularProvider) {
             RestangularProvider.setBaseUrl("/api/v1");
             RestangularProvider.setRequestSuffix("/");
-        }]);
-        // .config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
-        //     $routeProvider.when("/Book/:bookId", {
-        //         templateUrl: "book.html",
-        //         controller: "BookCtrl",
-        //         controllerAs: "book"
-        //     }).when("/Book/:bookId/ch/:chapterId", {
-        //         templateUrl: "chapter.html",
-        //         controller: "ChapterCtrl",
-        //         controllerAs: "chapter"
-        //     });
-        //
-        //     $locationProvider.html5Mode(true);
-        // }]);
+        }])
+        .config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+            $routeProvider.when("/", {
+                templateUrl: "main.html",
+                controller: "MainController"
+            }).otherwise({
+                redirectTo: "/"
+            });
 
-    trackie_module.controller("mainController", ["$scope", "Restangular", function ($scope, Restangular) {
-        $scope.login = function () {
-            Restangular.all("auth").customPOST({"username": "admin", "password": "root"}, "login");
+            //$locationProvider.html5Mode(true);
+        }]);
+
+    // Services
+
+    trackie_module.service("djangoAuth", ["$q", "$http", "$cookies", "$rootScope", "$templateCache", function ($q, $http, $cookies, $rootScope, $templateCache) {
+        return {
+            "API_URL": "api/v1/auth",
+            "use_session": true,
+            "authenticated": null,
+            "authPromise": null,
+            "request": function (args) {
+                if ($cookies.token) {
+                    $http.defaults.headers.common.Authorization = "Token " + $cookies.token;
+                }
+                args = args || {};
+                var deferred = $q.defer();
+                var url = this.API_URL + args.url;
+                var method = args.method || "GET";
+                var params = args.params || {};
+                var data = args.data || {};
+                $http({
+                    url: url,
+                    withCredentials: this.use_session,
+                    method: method.toUpperCase(),
+                    params: params,
+                    headers: {"X-Requested-With": "XMLHttpRequest"},
+                    data: data
+                }).success(angular.bind(this, function (data, status) {
+                    deferred.resolve(data, status);
+                })).error(angular.bind(this, function (data, status, headers, config) {
+                    if (data) {
+                        data.status = status;
+                    }
+                    if (status === 0) {
+                        if (data === "") {
+                            data = {};
+                            data.status = 0;
+                            data.non_field_errors = ["Could not connect. Please try again."];
+                        }
+                        if (data === null) {
+                            data = {};
+                            data.status = 0;
+                            data.non_field_errors = ["Server timed out. Please try again."];
+                        }
+                    }
+                    deferred.reject(data, status, headers, config);
+                }));
+                return deferred.promise;
+            },
+            "register": function (username, password1, password2, email, more) {
+                var data = {
+                    "username": username,
+                    "password1": password1,
+                    "password2": password2,
+                    "email": email
+                };
+                data = angular.extend(data, more);
+                return this.request({
+                    "method": "POST",
+                    "url": "/registration/",
+                    "data": data
+                });
+            },
+            "login": function (username, password) {
+                var djangoAuth = this;
+                return this.request({
+                    "method": "POST",
+                    "url": "/login/",
+                    "data": {
+                        "username": username,
+                        "password": password
+                    }
+                }).then(function (data) {
+                    if (!djangoAuth.use_session) {
+                        $http.defaults.headers.common.Authorization = "Token " + data.key;
+                        $cookies.token = data.key;
+                    }
+                    djangoAuth.authenticated = true;
+                    $rootScope.$broadcast("djangoAuth.logged_in", data);
+                });
+            },
+            "logout": function () {
+                var djangoAuth = this;
+                return this.request({
+                    "method": "POST",
+                    "url": "/logout/"
+                }).then(function () {
+                    delete $http.defaults.headers.common.Authorization;
+                    delete $cookies.token;
+                    djangoAuth.authenticated = false;
+                    $rootScope.$broadcast("djangoAuth.logged_out");
+                });
+            },
+            "changePassword": function (password1, password2) {
+                return this.request({
+                    "method": "POST",
+                    "url": "/password/change/",
+                    "data": {
+                        "new_password1": password1,
+                        "new_password2": password2
+                    }
+                });
+            },
+            "resetPassword": function (email) {
+                return this.request({
+                    "method": "POST",
+                    "url": "/password/reset/",
+                    "data": {
+                        "email": email
+                    }
+                });
+            },
+            "profile": function () {
+                return this.request({
+                    "method": "GET",
+                    "url": "/user/"
+                });
+            },
+            "updateProfile": function (data) {
+                return this.request({
+                    "method": "PATCH",
+                    "url": "/user/",
+                    "data": data
+                });
+            },
+            "verify": function (key) {
+                return this.request({
+                    "method": "POST",
+                    "url": "/registration/verify-email/",
+                    "data": {"key": key}
+                });
+            },
+            "confirmReset": function (uid, token, password1, password2) {
+                return this.request({
+                    "method": "POST",
+                    "url": "/password/reset/confirm/",
+                    "data": {
+                        "uid": uid,
+                        "token": token,
+                        "new_password1": password1,
+                        "new_password2": password2
+                    }
+                });
+            },
+            "authenticationStatus": function (restrict, force) {
+                restrict = restrict || false;
+                force = force || false;
+                if (this.authPromise === null || force) {
+                    this.authPromise = this.request({
+                        "method": "GET",
+                        "url": "/user/"
+                    });
+                }
+                var self = this;
+                var defer = $q.defer();
+                if (this.authenticated !== null && !force) {
+                    if (this.authenticated === false && restrict) {
+                        defer.reject("User is not logged in.");
+                    } else {
+                        defer.resolve();
+                    }
+                } else {
+                    this.authPromise.then(function () {
+                        if (!self.authenticated) {
+                            $templateCache.removeAll();
+                        }
+                        self.authenticated = true;
+                        defer.resolve();
+                    }, function () {
+                        if (self.authenticated) {
+                            $templateCache.removeAll();
+                        }
+                        self.authenticated = false;
+                        if (restrict) {
+                            defer.reject("User is not logged in.");
+                        } else {
+                            defer.resolve();
+                        }
+                    });
+                }
+                return defer.promise;
+            },
+            "initialize": function (url, sessions) {
+                this.API_URL = url || this.API_URL;
+                this.use_session = sessions || this.use_session;
+                return this.authenticationStatus();
+            }
         };
+    }]);
+
+    trackie_module.controller("MainController", ["$scope", "djangoAuth", "Restangular", function ($scope, djangoAuth, Restangular) {
+        djangoAuth.initialize();
     }]);
 }());
--- web/domain_app/trackie/views.py
+++ web/domain_app/trackie/views.py
@@ -6,14 +6,11 @@ from .forms import RegisterForm
 class HomePageView(views.TemplateView):
 
     name = 'trackie.home'
-    template_name = 'trackie/unregistered/home.html'
+    template_name = 'trackie/home.html'
     form = None
 
     def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated():
-            self.template_name = 'trackie/registered/home.html'
-        else:
-            self.form = RegisterForm()
-            kwargs['form'] = self.form
+        self.form = RegisterForm()
+        kwargs['form'] = self.form
 
         return super(HomePageView, self).get(request, *args, **kwargs)
