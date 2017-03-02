54331ebfcd17743926c03013d3fdb72432ccf0be
Redpoint1 <Redpoints1@gmail.com>
2017-03-02 12:33:48 +0100

    Added CRU_ API for Racer model

--- web/domain_app/trackie/rest/v1/models/racer/views.py
+++ web/domain_app/trackie/rest/v1/models/racer/views.py
@@ -10,5 +10,11 @@ class RacerViewSet(ModelViewSet):
     def list(self, request, *args, **kwargs):
         return super(RacerViewSet, self).list(request, *args, **kwargs)
 
+    def create(self, request, *args, **kwargs):
+        return super(RacerViewSet, self).create(request, *args, **kwargs)
+
     def retrieve(self, request, *args, **kwargs):
         return super(RacerViewSet, self).retrieve(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        return super(RacerViewSet, self).update(request, *args, **kwargs)
