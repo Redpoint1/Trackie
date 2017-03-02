c91a8b5c1e5c882735053d7a1e3e121b38e285db
Redpoint1 <Redpoints1@gmail.com>
2017-03-02 12:32:28 +0100

    Simplified Racer serializers, added photo fiel as Base64

--- web/domain_app/trackie/rest/v1/models/racer/serializers.py
+++ web/domain_app/trackie/rest/v1/models/racer/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework.serializers import (HyperlinkedModelSerializer,
                                         SerializerMethodField)
+from drf_extra_fields.fields import Base64ImageField
 from ......trackie.models import RacerInRace, Racer
 
 
-class RaceRacerSerializer(HyperlinkedModelSerializer):
+class RacerSerializer(HyperlinkedModelSerializer):
+    photo = Base64ImageField(required=False)
+
+    class Meta:
+        model = Racer
+        fields = "__all__"
+
+
+class RaceRacerSerializer(RacerSerializer):
     number = SerializerMethodField("get_num")
 
     def get_num(self, instance):
@@ -11,13 +20,5 @@ class RaceRacerSerializer(HyperlinkedModelSerializer):
         racer_rel = RacerInRace.objects.get(racer=instance, race=item)
         return racer_rel.number
 
-    class Meta:
-        model = Racer
-        fields = "__all__"
-
-
-class RacerSerializer(HyperlinkedModelSerializer):
-
-    class Meta:
-        model = Racer
-        fields = "__all__"
+    class Meta(RacerSerializer.Meta):
+        pass
