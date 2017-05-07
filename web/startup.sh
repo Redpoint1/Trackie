4185f440d9580b89c8d0a839dff4ae62e30064f2
Redpoint1 <Redpoints1@gmail.com>
2017-05-07 03:08:55 +0200

    working docker-compose

--- web/startup.sh
+++ web/startup.sh
@@ -1,1 +1,1 @@
-python manage.py collectstatic --noinput && /usr/local/bin/uwsgi --yaml ./trackie/uwsgi.yml
+python manage.py migrate && python manage.py collectstatic --noinput && /usr/local/bin/uwsgi --yaml ./trackie/uwsgi.yml --wsgi-file trackie/wsgi.py --enable-threads
